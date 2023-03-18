6a7907fd0e0e7188488d2aa3d47686bfe04e53ce
Akashdeep Mahata <90324172+AKASHDEEPMAHATA@users.noreply.github.com>
2023-03-19 00:04:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def home():
         return render_template("index.html", movie_list = movie_list, status = status)
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False , host="0.0.0.0")
